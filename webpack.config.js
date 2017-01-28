e6bf359b7af29ecdc3edb90755601aef0c706f7e
Ngaosathe <ThekhoN@users.noreply.github.com>
2017-01-28 14:05:12 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           },
           {
             test:/\.css$/,
-            excluse: /node_modules/,
+            exclude: /node_modules/,
             loader: 'style!css'
           }
       ],
